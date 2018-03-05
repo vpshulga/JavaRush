faa4bedcc0aa277093a066c348ae15bb0b10e69d
vpshulga <vpshulgaa@gmail.com>
2018-03-05 14:12:46 +0300

    "Snake 10"

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -1,7 +1,63 @@
 package com.javarush.task.task23.task2312;
 
 public class Room {
+    private int width;
+    private int height;
+    private Snake snake;
+
+    public static Room game;
+
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+    private Mouse mouse;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
     public static void main(String[] args) {
+        game = new Room(400, 400, new Snake(0,0));
+        game.getSnake().setDirection(SnakeDirection.DOWN);
+    }
+
+    void run(){
+
+    }
+
+    void print(){
 
     }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,4 +1,43 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public Snake(int x, int y){
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    int getX(){
+        return sections.get(0).getX();
+    }
+    int getY(){
+        return sections.get(0).getY();
+    }
+
+    void move(){
+        
+    }
 }
