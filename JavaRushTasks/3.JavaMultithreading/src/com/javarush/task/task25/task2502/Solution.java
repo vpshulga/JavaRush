722325cc8f4b553888397510bb79e67d7df9877c
vpshulga <vpshulgaa@gmail.com>
2018-03-09 16:31:12 +0300

    "Lvl25"

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws Exception {
+
+                wheels = new ArrayList<>();
+                for (int i = 0; i < loadWheelNamesFromDB().length; i++) {
+                    wheels.add(Wheel.valueOf(loadWheelNamesFromDB()[i]));
+                }
+
+                if (wheels.size() != 4)
+                    throw new Exception();
+
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
